41834a3216441526b6d55c15391afb9eaa88aa25
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-03 18:24:31 +0200

    Add eastwood

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-cljfmt "0.5.7"]]
-        :dependencies [[cljfmt "0.5.1"]]
+        :dependencies [[cljfmt "0.5.1"]
+                       [jonase/eastwood "0.2.1"
+                        :exclusions [org.clojure/clojure]]]
         :repl-options {:init (require 'cljfmt.core)}}}
