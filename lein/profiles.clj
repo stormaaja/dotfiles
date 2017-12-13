db5078b4f9673b9b08585e8ab0828893cefd8b74
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-13 10:36:27 +0200

    Add cljfm rule

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
         :dependencies [[cljfmt "0.5.1"]
                        [jonase/eastwood "0.2.1"
                         :exclusions [org.clojure/clojure]]]
-        :repl-options {:init (require 'cljfmt.core)}}}
+        :repl-options {:init (require 'cljfmt.core)}
+        :cljfmt {:indents ^:replace {#".*" [[:inner 0]]}}}}
