f5aa750a51c966cfb6af04624738c99570e1555e
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-13 09:39:42 +0200

    Remove forced dependencies (emacs cider will handle this)

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,1 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.1"]
-                  [refactor-nrepl "2.3.1"]
-                  [lein-zprint "0.1.16"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]
-        :zprint {:old? false}}}
+{:user {:pedantic? :warn}}
