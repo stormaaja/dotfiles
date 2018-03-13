f6c87c5a300c54eba8dd12e94269bc947eade962
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-13 09:34:38 +0200

    Add emacs config

---
+++ emacs/init.el
@@ -0,0 +1,327 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar ido-cur-item nil)
+(defvar ido-default-item nil)
+(defvar ido-cur-list nil)
+(defvar predicate nil)
+(defvar inherit-input-method nil)
+
+(defvar my-packages
+  '(
+    ace-window
+    avy
+    cider
+    clojure-mode-extra-font-locking
+    clojure-mode
+    column-marker
+    exec-path-from-shell
+    fill-column-indicator
+    highlight-indent-guides
+    ido-ubiquitous
+    ido-completing-read+
+    magit
+    git-commit
+    ghub
+    let-alist
+    magit-popup
+    markdown-mode
+    memoize
+    paredit
+    projectile
+    pkg-info
+    epl
+    queue
+    rainbow-delimiters
+    seq
+    smex
+    spinner
+    tagedit
+    dash
+    s
+    with-editor async
+    json-mode
+    xclip
+    blank-mode
+    ))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs
+   '("PATH")))
+
+(require 'xclip)
+(xclip-mode 1)
+;(turn-on-xclip)
+
+;; Navigation
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(setq recentf-save-file (concat user-emacs-directory ".recentf"))
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 40)
+
+(ido-mode t)
+
+(setq ido-enable-flex-matching t)
+
+(setq ido-use-filename-at-point nil)
+
+(setq ido-auto-merge-work-directories-length -1)
+
+(setq ido-use-virtual-buffers t)
+
+(ido-ubiquitous-mode 1)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x C-o") 'ace-window)
+
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+
+(projectile-global-mode)
+
+;; UI
+(require 'fill-column-indicator)
+(setq fci-rule-width 1)
+(setq fci-rule-color "darkgray")
+(setq fci-rule-column 80)
+(add-hook 'after-change-major-mode-hook 'turn-on-fci-mode)
+
+(menu-bar-mode -1)
+
+(global-linum-mode)
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(add-to-list 'load-path "~/.emacs.d/themes")
+
+(load-theme 'tomorrow-night-bright t)
+
+(set-face-attribute 'default nil :height 140)
+
+(setq
+      x-select-enable-clipboard t
+      x-select-enable-primary t
+      save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t)
+
+(blink-cursor-mode 0)
+
+(setq-default frame-title-format "%b (%f)")
+
+(global-set-key (kbd "s-t") '(lambda () (interactive)))
+
+(setq ring-bell-function 'ignore)
+
+;; Editing
+(global-set-key (kbd "M-/") 'hippie-expand)
+(standard-display-ascii ?\t "^I")
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
+(show-paren-mode 1)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq-default fill-column 80)
+
+(global-hl-line-mode 1)
+
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(setq-default indent-tabs-mode nil)
+
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (concat user-emacs-directory "places"))
+
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+(setq auto-save-default nil)
+
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+(defun die-tabs ()
+  (interactive)
+  (set-variable 'tab-width 2)
+  (mark-whole-buffer)
+  (untabify (region-beginning) (region-end))
+  (keyboard-quit))
+
+(setq electric-indent-mode nil)
+
+(setq highlight-indent-guides-method 'character)
+
+;; Misc
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+(setq create-lockfiles nil)
+
+(setq inhibit-startup-message t)
+
+;; Elisp
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; eldoc-mode shows documentation in the minibuffer when writing code
+;; http://www.emacswiki.org/emacs/ElDoc
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+;; Clojure
+
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+(add-hook 'clojure-mode-hook 'subword-mode)
+
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+(require 'clojure-mode-extra-font-locking)
+
+(defun clj-mode ()
+  (setq inferior-lisp-program "lein repl")
+  (font-lock-add-keywords
+   nil
+   '(("(\\(facts?\\)"
+      (1 font-lock-keyword-face))
+     ("(\\(background?\\)"
+      (1 font-lock-keyword-face))))
+  (define-clojure-indent (fact 1))
+  (define-clojure-indent (facts 1))
+  (local-set-key (kbd "RET") 'newline-and-indent))
+
+(add-hook 'clojure-mode-hook 'clj-mode)
+
+(setq clojure-indent-style :align-arguments)
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'enable-paredit-mode)
+(add-hook 'cider-mode-hook 'turn-on-fci-mode)
+(add-hook 'cider-mode-hook 'subword-mode)
+(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook 'clj-mode)
+
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+(setq cider-repl-wrap-history t)
+
+(add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
+
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+;(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
+
+(defun cider-start-http-server ()
+  (interactive)
+  (cider-load-current-buffer)
+  (let ((ns (cider-current-ns)))
+    (cider-repl-set-ns ns)
+    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+
+
+(defun cider-refresh ()
+  (interactive)
+  (cider-interactive-eval (format "(user/reset)")))
+
+(defun cider-user-ns ()
+  (interactive)
+  (cider-repl-set-ns "user"))
+
+(eval-after-load 'cider
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
+     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
+     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
+     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+
+
+;; JS
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-hook 'js-mode-hook 'subword-mode)
+(add-hook 'html-mode-hook 'subword-mode)
+(setq js-indent-level 2)
+(eval-after-load "sgml-mode"
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
+
+;; Latex
+
+(setq TeX-quote-after-quote 0)
+
+;; Other
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(frame-background-mode (quote dark))
+ '(package-selected-packages
+   (quote
+    (blank-mode highlight-chars json-mode ace-window fill-column-indicator markdown-mode solarized-theme column-marker highlight-indent-guides rainbow-delimiters tagedit smex projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
