c12f1bad2b23338ef314aa9fb76b84be8d26d1c9
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-05 08:58:58 +0300

    Fix tide with typescript

--- emacs/user.el
+++ emacs/user.el
@@ -58,6 +58,7 @@
     spinner
     tagedit
     tide
+    web-mode
     with-editor async
     xclip
     zenburn
@@ -380,10 +381,14 @@
   (when (string-equal "tsx" (file-name-extension buffer-file-name))
     (tide-common-setup)))
 
-(add-hook 'web-mode-hook #'tide-tsx-setup)
-
+(require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
 
+;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
 
 ;;; TypeScript: .jsx sources
