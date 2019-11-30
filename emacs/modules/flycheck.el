d5d5caeb0aa8af1441d3759523c8ef20eec76e45
Matti Ahinko <matti@ahinko.net>
2019-11-30 18:31:44 +0200

    Add flycheck tips

--- emacs/modules/flycheck.el
+++ emacs/modules/flycheck.el
@@ -1,5 +1,8 @@
-(use-packages '(flycheck))
+(use-packages '(flycheck flycheck-popup-tip))
 
 (global-flycheck-mode)
 
 ;(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(with-eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
