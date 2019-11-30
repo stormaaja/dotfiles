ba4ea475e10373864ce381fafa6e62b5d0ad0a7b
Matti Ahinko <matti@ahinko.net>
2019-11-30 18:32:00 +0200

    Add paths for linux as well

--- emacs/modules/linux.el
+++ emacs/modules/linux.el
@@ -1,6 +1,9 @@
-;; Xclip for Linux
+;; Linux
 
-(use-packages '(xclip))
+(use-packages '(xclip exec-path-from-shell))
 
 (require 'xclip)
 (xclip-mode 1)
+
+(exec-path-from-shell-initialize)
+(exec-path-from-shell-copy-envs '("PATH"))
