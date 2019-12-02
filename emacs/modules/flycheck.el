9e24a6fd5c019254858dabf4fbb3bf16a014db53
stormaaja <matti.ahinko@gmail.com>
2019-12-02 15:17:38 +0200

    Remove tooltip

--- emacs/modules/flycheck.el
+++ emacs/modules/flycheck.el
@@ -1,8 +1,5 @@
-(use-packages '(flycheck flycheck-popup-tip))
+(use-packages '(flycheck))
 
 (global-flycheck-mode)
 
 ;(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(with-eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
