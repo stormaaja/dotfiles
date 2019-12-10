ee7072822b83714e910d444d8c8383917b2c8e9b
stormaaja <matti.ahinko@gmail.com>
2019-12-10 15:57:19 +0200

    Add toggling dark/light theme

--- emacs/modules/theme.el
+++ emacs/modules/theme.el
@@ -1,2 +1,13 @@
 (use-packages '(solarized-theme))
-(load-theme 'solarized-dark t)
+
+(setq custom/theme-dark-enabled nil)
+
+(defun toggle-theme ()
+  (setq custom/theme-dark-enabled (not custom/theme-dark-enabled))
+  (if custom/theme-dark-enabled
+      (load-theme 'solarized-dark t)
+    (load-theme 'solarized-light t)))
+
+(global-set-key (kbd "C-x t") (lambda () (interactive) (toggle-theme)))
+
+(toggle-theme)
