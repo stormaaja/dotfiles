f1b725308a31088ba5d5b50104beea1eb2e7e376
Matti Ahinko <matti.ahinko@gmail.com>
2026-03-14 10:04:16 +0200

    Change to tokyo night

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,17 @@
 -- Layout
+
+require "paq" {
+    "savq/paq-nvim", -- Let Paq manage itself
+    "neovim/nvim-lspconfig",
+    { "lervag/vimtex", opt = true }, -- Use braces when passing options
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+		"folke/tokyonight.nvim",
+}
+
 vim.o.number = true
 vim.o.hlsearch = false
 vim.o.signcolumn = 'yes'
-vim.cmd.colorscheme('retrobox')
+vim.cmd.colorscheme('tokyonight')
 -- Tabs
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
