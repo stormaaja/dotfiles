8478ca730b0da57226e00c5923c8122e31e229ee
Matti Ahinko <matti.ahinko@gmail.com>
2026-05-18 13:52:18 +0300

    Add telescope commands

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,3 +29,13 @@ vim.o.smartcase = true
 vim.o.ignorecase = true
 -- Copy to system clipboard
 vim.o.clipboard = "unnamedplus"
+
+-- Telescope config
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, { desc = 'Telescope find definitions' })
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Telescope find references' })
+
